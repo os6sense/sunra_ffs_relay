c1a71488cde66d0ec2500a1fa5da97d5b6416f88
os6sense <leej@sowhatresearch.com>
2014-09-22 10:42:30 +0100

    Updated service namespace

--- sunra-ffserver-relay.rb
+++ sunra-ffserver-relay.rb
@@ -5,7 +5,7 @@
 # Description::
 # Wraps around Sunra::FFServer::Relay allowing the relay process to
 # be started and stopped. NOT a daemon since ffserver and ffmpeg
-# are spawned processes. If a file is being relayed it will loop 
+# are spawned processes. If a file is being relayed it will loop
 # since this will be detected as the feed dying and the relay server
 # will restart the feed from the begining.
 
@@ -13,7 +13,7 @@
 require 'sunra_service'
 require_relative 'ffserver-relay'
 
-include SunraService
+include Sunra::Utils::Service
 
 service_name = 'sunra-ffsrelay-server.rb'
 usage if ARGV.length != 1
